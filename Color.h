96f01bfef72db14d44e51715f378af933063b230
Joakim Rasmuson <joakimrasmuson@gmail.com>
2020-01-04 13:18:18 +0100

    Improved color struct

--- Color.h
+++ Color.h
@@ -18,14 +18,36 @@ typedef struct Color {
     this->g = g;
     this->b = b;
   }
+
+  static Color add(Color a, Color b) {
+    return Color(a.r + b.r, a.g + b.g, a.b + b.b);
+  }
+
+  static Color multiply(Color c, float f) {
+    return Color(f * c.r, f * c.g, f * c.b);
+  }
+
+  static Color interpolate(Color a, Color b, float t){
+    
+    if (t <= 0)
+      return a;
+    else if (t >= 1.0f)
+      return b;
+
+    return add(multiply(a, 1.0f - t), multiply(b, t));
+  }
 };
 
-Color operator *(float f, const Color& c) {
-  return Color(f * c.r, f * c.g, f * c.b);
+Color operator +(const Color& a, const Color& b) {
+  return Color::add(a, b);
 }
 
-Color operator* (const Color& c, float f) {
-  return Color(f * c.r, f * c.g, f * c.b);
+Color operator *(float f, const Color& c) {
+  return Color::multiply(c, f);
+}
+
+Color operator *(const Color& c, float f) {
+  return Color::multiply(c, f);
 }
 
 #endif
